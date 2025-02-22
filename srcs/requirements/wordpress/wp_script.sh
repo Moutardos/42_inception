93eda5299af7e89e8d88da5cfa04a1ca376313cf
lcozdenm <loic.cozdenmat@gmail.com>
2025-02-22 10:28:49 -0500

    FINISHED

--- srcs/requirements/wordpress/wp_script.sh
+++ srcs/requirements/wordpress/wp_script.sh
@@ -9,14 +9,17 @@ if [ ! -f ${WWW_DIR}/.done ]; then
     echo "Current directory: $(pwd)"
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
-	chown -R www-data:0 ${WWW_DIR}
-	mv wp-cli.phar wp
-	./wp --info
-	./wp core download --allow-root
-	./wp config create --dbname=${MARIA_DB_NAME} --dbuser=${MARIA_DB_USERNAME} --dbpass=${MARIA_DB_PASSWORD} --dbhost=mariadb:3306 --allow-root
-	./wp core install --url=https://${SERVER_NAME} --title="Inception" --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_MAIL} --allow-root
-	./wp user create ${WP_USER_NAME} ${WP_USER_MAIL} --user_pass=${WP_USER_PASSWORD} --allow-root
-	./wp user list --fields=ID,user_login,roles --allow-root
+	./wp-cli.phar --info
+	./wp-cli.phar core download --allow-root
+	./wp-cli.phar config create --dbname=${MARIA_DB_NAME} --dbuser=${MARIA_DB_USERNAME} --dbpass=${MARIA_DB_PASSWORD} --dbhost=mariadb:3306 --allow-root
+	./wp-cli.phar core install --url=https://${SERVER_NAME} --title="Inception" --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_MAIL} --allow-root
+	./wp-cli.phar user create ${WP_USER_NAME} ${WP_USER_MAIL} --user_pass=${WP_USER_PASSWORD} --allow-root
+	./wp-cli.phar rewrite structure '/%year%/%monthnum%/%postname%/' --allow-root
+	./wp-cli.phar user list --fields=ID,user_login,roles --allow-root
+	chown -R www-data:www-data ${WWW_DIR}
+	chmod -R g+w ${WWW_DIR}
+
+	# chmod -R 664 ${WWW_DIR}
 	touch ${WWW_DIR}/.done $
 	echo "finished"
 else
